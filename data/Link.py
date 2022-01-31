8884315f094e2f6c614f9a4efba23026359ac668
pankov <frostkslo1@yandex.ru>
2022-01-31 20:10:59 +0300

    Kirrill's commits

--- data/Link.py
+++ data/Link.py
@@ -1,16 +1,11 @@
 class Link:
-	_counter = 0
-	def __init__(self):
-		Link._counter += 1
-		self.id = _counter
-		self.link = 1
-		self.task_id = None
-
-	def __init__(self, dict_):
-		Link._counter += 1
-		self.id = dict_["_id"]
-		self.link = dict_["link"]
-		self.task_id = dict_["task_id"]
+	def __init__(self, dict_ = None):
+		if(dict_ is None):
+			self.link = 1
+			self.task_id = None
+		else:
+			self.link = dict_["link"]
+			self.task_id = dict_["task_id"]
 
 	def asdict(self):
-		return {"id": self.id, "link": self.link, "task_id": self.task_id}
+		return {"link": self.link, "task_id": self.task_id}
